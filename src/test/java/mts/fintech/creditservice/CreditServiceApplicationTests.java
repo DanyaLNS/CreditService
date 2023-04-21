06fe56272abd97ed2d28055d6648feed54144e9a
Danila Anikin <danil-anikin-98@mail.ru>
2023-04-21 20:17:42 +0300

    Initial commit

---
+++ src/test/java/mts/fintech/creditservice/CreditServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package mts.fintech.creditservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
